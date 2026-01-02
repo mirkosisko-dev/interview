68ca783c4260b500746d77df2b84878fba162759
mirkosisko-dev <mirkosisko.dev@gmail.com>
2026-01-02 17:47:56 +0100

    fix

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import orderRouter from "./routers/order-router";
-import { errorHandler } from "./middleware/error-handler";
 
 const app = express();
 
@@ -9,7 +8,5 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/orders", orderRouter);
-
-app.use(errorHandler);
 
 app.listen(8080, () => console.log("server listening on port 8080"));
